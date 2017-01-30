cea6a5852ac97c8103aa4e6cb57ed5a9e03dc042
amandayoung <ayoung18@hb.edu>
2017-01-29 23:36:23 -0500

    Add files via upload

--- StringArrayTools.java
+++ StringArrayTools.java
@@ -1,19 +1,18 @@
-/** The class IntegerArrayTools implements a variety of basic array tasks
-  * using an array of integers.
+/** The class StringArrayTools implements a variety of basic array tasks
+  * using an array of strings.
   * 
   * The array has a fixed length MAX_NUMS but the actual number of elements
   * that are used can vary.
   * 
   * @author Amanda Young 
-  * 
   */
 
 import java.util.Scanner;
 
-public class IntegerArrayTools
+public class StringArrayTools
 {
   
-  private int[] arrayData;
+  private String[] arrayData;
   private int arrayCount;
   public final int MAX_NUMS = 100;
   
@@ -23,15 +22,15 @@ public class IntegerArrayTools
     * the fillRandom or fillKeyboard methods.
     */
   
-  public IntegerArrayTools ()
+  public StringArrayTools()
   {
-    arrayData = new int[MAX_NUMS];
+    arrayData = new String[MAX_NUMS];
     arrayCount = 0;
   }
   
   /** Returns the number of objects in the array.
     * @return the count of items in the array. */
-  public int getCount ()
+  public int getCount()
   {
     return arrayCount;
   }
@@ -41,11 +40,11 @@ public class IntegerArrayTools
     * @param n The element to add
     * @return whether the element was successfully added
     */
-  public boolean addItem (int n)
+  public boolean addItem (String s)
   {
     if (arrayCount < MAX_NUMS)
     {
-      arrayData[arrayCount] = n;
+      arrayData[arrayCount] = s;
       arrayCount++;
       return true;
     }
@@ -57,29 +56,31 @@ public class IntegerArrayTools
     * @param n Upper bound for numbers with which to fill the array.
     * @param count the array to fill.  Returns a modified array.
     */
+  /**
   public void fillRandom (int n, int count) // fill random up to 100 and 3 (100,3) n = 100 count = 3 
   {
     if (count > MAX_NUMS)       // check that it's not too many items
       count = MAX_NUMS;
     for (int i = 0; i < count; i++)
-      addItem((int)(Math.random() * n) + 1);
+      addItem((string)(Math.random() * s) + 1);
   }
+  */
   
-  /** Fills the array with integers entered by the user
+  /** Fills the array with strings entered by the user
     * @param nums the array to fill.  Returns a modified array.
     */
   public void fillKeyboard ()
   {
     Scanner keyboard = new Scanner(System.in);
-    System.out.println ("Please enter integers on one line, separated by spaces.");
-    System.out.println ("Use -1 to signal the end of the list.");
+    System.out.println ("Please enter new strings on different lines.");
+    System.out.println ("Use a string with length 0 to signal the end of the list.");
     
     arrayCount = 0;
-    int n = keyboard.nextInt();
-    while (n != -1)                 // stop input when user enters -1
+    String s = keyboard.nextLine();
+    while (s.length() != 0)                 // stop input a string with a lenth of 0 
     {
-      addItem(n);
-      n = keyboard.nextInt();
+      addItem(s);
+      s = keyboard.nextLine();
     }
     keyboard.close();
   }
@@ -90,66 +91,64 @@ public class IntegerArrayTools
   public void printArray ()
   {
     for (int i = 0; i < arrayCount; i++)
-      System.out.print (arrayData[i] + "  ");
-    System.out.println();
+      System.out.print (arrayData[i]);
   }
   
   /** Finds the sum of an integer array
     * @return the sum of the elements in the array
     */
+  /**
   public int sumArray ()
   {
     int sum = 0;
     for (int i = 0; i < arrayCount; i++)
-      sum = sum + arrayData[i];
+      sum = sum + arrayData[i]; // doesnt make sense with strings 
     return sum;
   }
+  **/
   
-  
-  /** Finds the largest number of the integer array
-    * @retirn the largest number
+  /** Finds the largest String of the array
+    * @return the largest string in the array 
     **/
     
-  public int getMax () 
+  public String getMax () 
 {
-  int MaxNum = arrayData [0];
-  for (int i=0; i<arrayCount; i++) 
+  String MaxString = arrayData [0];
+  for (int i=1; i<arrayCount; i++) 
   {
-    if (arrayData [i] > MaxNum)
-          MaxNum = arrayData [i];
+    if (arrayData[i].compareTo(MaxString) > 0)
+          MaxString = arrayData[i];
   }
-return MaxNum;
+return MaxString;
 }
 
  
   /** Finds the smallest number of the integer array
     * @return the smallest number 
     **/
-  public int getMin () 
+  public String getMin() 
 {
-  int minNum = arrayData [0];
+  String minString = arrayData[0];
   for (int i=0; i<arrayCount; i++) 
   {
-    if (arrayData [i] > minNum)
-          minNum = arrayData [i];
+    if (arrayData[i].compareTo(minString) < 0)
+          minString = arrayData [i];
   }
-return minNum;
+return minString;
 }
   
-  /** Finds the index of the largest number of the integer array 
-    * @return the index of the largest number 
+  /** Finds the location of the mac string in the array
+    * @return the location of the max string in the array  
     **/
   
   public int getMaxIndex() 
   {
-    int max = arrayData[0];
     int n = 0;
     
     for (int i =1; i <arrayCount; i++)
     {
-      if (arrayData[i] > max) 
+      if (arrayData[i].compareTo(arrayData[n]) > 0) //same as the original get max
       {
-        max = arrayData [i];
         n = i; 
       }
     }
@@ -158,20 +157,18 @@ return minNum;
   
   
 
-  /** Find the index of the smallest number of the integer array 
-    * @return the index of the smallest number 
+  /** Find the min strings of the integer array 
+    * @return the min string of the array 
     **/
   
  public int getMinIndex()
-  {
-    int min = arrayData[0];
+   {
     int n = 0;
     
-    for (int i = 1; i < arrayCount; i++)
+    for (int i =1; i <arrayCount; i++)
     {
-      if (arrayData[i] < min)
+      if (arrayData[i].compareTo(arrayData[n]) < 0) //same as the original get min
       {
-        min = arrayData[i];
         n = i; 
       }
     }
@@ -183,12 +180,12 @@ return minNum;
   /** Tests the various array tools */
   public static void main (String[] args)
   {
-    IntegerArrayTools myArray = new IntegerArrayTools();
+    StringArrayTools myArray = new StringArrayTools();
     //myArray.fillRandom(100, 12);
     myArray.fillKeyboard ();
     myArray.printArray ();
-    System.out.println ("There are " + myArray.getCount() + " integers in the array.");
-    System.out.println ("The sum is " + myArray.sumArray());
+    System.out.println ("There are " + myArray.getCount() + " strings in the array.");
+    //System.out.println ("The sum is " + myArray.sumArray());
     
     // Un-comment these lines one at a time after you have written the appropriate code
        System.out.println ("The largest item is " + myArray.getMax() ); 
@@ -199,16 +196,3 @@ return minNum;
     
   }
 }
-
-
-
-      
-  
-  
-  
-  
-
-    
-
-
-    
